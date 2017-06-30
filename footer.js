efa3b4ab0a3dc0cbd7372de97f1ce076d779b569
Riley Grigg <griggriley@gmail.com>
2017-06-30 15:21:57 -0600

    added Facebook like button

--- footer.js
+++ footer.js
@@ -1,2 +1,3 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;2017 - " + new Date().getFullYear() + " Riley Grigg</p>"
+"<p>&copy;2017 - " + new Date().getFullYear() + " Riley Grigg</p>
+<div class="fb-like" data-href="https://griggril000.github.io/rileys-bootstrap-blog/posts/2017/june/bunny-face-paint.html" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>"
