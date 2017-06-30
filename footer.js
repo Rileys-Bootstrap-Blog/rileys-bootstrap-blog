14a0e06e852e49fe69f3d20fec64ed0aeea655d2
Riley Grigg <griggriley@gmail.com>
2017-06-30 15:23:25 -0600

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,3 +1,2 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;2017 - " + new Date().getFullYear() + " Riley Grigg</p>
-<div class="fb-like" data-href="https://griggril000.github.io/rileys-bootstrap-blog/posts/2017/june/bunny-face-paint.html" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>"
+"<p>&copy;2017 - " + new Date().getFullYear() + " Riley Grigg </p> <div class="fb-like" data-href="https://griggril000.github.io/rileys-bootstrap-blog/posts/2017/june/bunny-face-paint.html" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div> "
