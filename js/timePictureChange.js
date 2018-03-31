bb2f10a3e8a32f8ddbef26a4824fc9a3216f3c49
Riley Grigg <griggriley@gmail.com>
2018-03-30 19:47:24 -0600

    Update timePictureChange.js

--- js/timePictureChange.js
+++ js/timePictureChange.js
@@ -6,8 +6,8 @@ Created by: Anonymous | http://musikimiz.googlepages.comLicensed under: Creative
 function pixTimeChange() {
   var t=new Date();
   var h = t.getHours();
-  var r1="/img/640A2BA5-76CE-4254-8CFF-6150BE695475.jpeg";
-  var r2="/img/216B7378-420C-4B55-881B-1160D1F24FFC.jpeg";
+  var r1="/img/CE9A9821-D952-426F-A34F-7D6CBBE0EDC4.jpeg";
+  var r2="/img/DD5C6A36-5C3D-41EE-ADBF-A2748A24CA5F.jpeg";
   var el=document.getElementById('myimage');
 
   // See the time below. Note: The time is in 24 hour format.
