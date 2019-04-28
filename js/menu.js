d429adb4cd103aa130b82f3373a24171635705c5
Riley Grigg <griggriley@gmail.com>
2019-04-28 15:13:57 -0600

    Delete menu.js

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-//creates the top menu
-function createMenu() {
-    document.getElementsByClassName('navbar-brand').innerHTML = '<a href=/>Riley\'s Bootstrap Blog</a>';
-    document.getElementById('home').innerHTML = '<li> <a href=/ title=Go Home (ALT-H) accesskey=h>Home</a></li>';
-}
